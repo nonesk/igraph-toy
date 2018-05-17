25c07baa87f97ca0ba8b7541ad9c4804789ef32f
Louis Duchemin <louis.duchemin@etu.univ-lyon1.fr>
2018-05-17 17:17:07 +0200

    advanced igraph exploration

--- main.cpp
+++ main.cpp
@@ -1,44 +1,92 @@
 #include <igraph.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
-int main(void) {
-     igraph_t graph;
-     igraph_vector_t v;
-     igraph_vector_t result;
-     igraph_real_t edges[] = { 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0, 7, 0, 8,
-                               0,10, 0,11, 0,12, 0,13, 0,17, 0,19, 0,21, 0,31,
-                               1, 2, 1, 3, 1, 7, 1,13, 1,17, 1,19, 1,21, 1,30,
-                               2, 3, 2, 7, 2,27, 2,28, 2,32, 2, 9, 2, 8, 2,13,
-                               3, 7, 3,12, 3,13, 4, 6, 4,10, 5, 6, 5,10, 5,16,
-                               6,16, 8,30, 8,32, 8,33, 9,33,13,33,14,32,14,33,
-                              15,32,15,33,18,32,18,33,19,33,20,32,20,33,
-                              22,32,22,33,23,25,23,27,23,32,23,33,23,29,
-                              24,25,24,27,24,31,25,31,26,29,26,33,27,33,
-                              28,31,28,33,29,32,29,33,30,32,30,33,31,32,31,33,
-                              32,33
-     };
+int main(void)
+{
+    igraph_i_set_attribute_table(&igraph_cattribute_table);
 
-     igraph_vector_view(&v, edges, sizeof(edges)/sizeof(double));
-     igraph_create(&graph, &v, 0, IGRAPH_UNDIRECTED);
+    igraph_t graph;
+    igraph_vector_t v;
+    igraph_vector_t result;
+    igraph_real_t edges[] = {
+        0, 1,
+        0, 2,
+        0, 3,
+        0, 4,
+        1, 5,
+        5, 6,
+        5, 7,
+        7, 8};
+    
+    bool t[] = {true, false, false, false, false, true, false, false, true};   
+    igraph_vector_bool_t types;
 
-     igraph_vector_init(&result, 0);
+    std::vector<std::string> strattrs = {"red", "blue","blue","blue","blue","red","blue","blue","red"};
+    igraph_strvector_t colors;
+    //igraph_attribute_table_t* attrs;
 
-     igraph_degree(&graph, &result, igraph_vss_all(), IGRAPH_ALL,
-                   IGRAPH_LOOPS);
-     printf("Maximum degree is      %10i, vertex %2i.\n",
-            (int)igraph_vector_max(&result), (int)igraph_vector_which_max(&result));
+    igraph_vector_view(&v, edges, sizeof(edges) / sizeof(double));
+    //igraph_vector_view(&types, t, sizeof(t) / sizeof(bool));
+    
+    igraph_vector_bool_init(&types, sizeof(t));
+    for(int i = 0; i<igraph_vector_bool_size(&types); ++i){
+        VECTOR(types)[i] = t[i];
+    }
+    igraph_strvector_init(&colors, sizeof(t));
+    int i = 0;
+    for(auto it: strattrs){
+        igraph_strvector_set(&colors, i, it.c_str());
+        ++i;
+    }
 
-     igraph_closeness(&graph, &result, igraph_vss_all(), IGRAPH_ALL,
-                      /*weights=*/ 0, true);
-     printf("Maximum closeness is   %10f, vertex %2i.\n",
-             (double)igraph_vector_max(&result), (int)igraph_vector_which_max(&result));
+    
+    igraph_create_bipartite(&graph, &types, &v, IGRAPH_UNDIRECTED);
 
-     igraph_betweenness(&graph, &result, igraph_vss_all(),
-                        IGRAPH_UNDIRECTED, /*weights=*/ 0, /*nobigint=*/ 1);
-     printf("Maximum betweenness is %10f, vertex %2i.\n",
-             (double)igraph_vector_max(&result), (int)igraph_vector_which_max(&result));
+    igraph_cattribute_VAS_setv(&graph, "color", &colors);
 
-     igraph_vector_destroy(&result);
-     igraph_destroy(&graph);
+    igraph_vector_init(&result, 0);
 
-     return 0;
+    igraph_degree(&graph, &result, igraph_vss_all(), IGRAPH_ALL,
+                  IGRAPH_LOOPS);
+    printf("Maximum degree is      %10i, vertex %2i.\n",
+           (int)igraph_vector_max(&result), (int)igraph_vector_which_max(&result));
+
+    igraph_closeness(&graph, &result, igraph_vss_all(), IGRAPH_ALL,
+                     /*weights=*/0, true);
+    printf("Maximum closeness is   %10f, vertex %2i.\n",
+           (double)igraph_vector_max(&result), (int)igraph_vector_which_max(&result));
+
+    igraph_betweenness(&graph, &result, igraph_vss_all(),
+                       IGRAPH_UNDIRECTED, /*weights=*/0, /*nobigint=*/1);
+    printf("Maximum betweenness is %10f, vertex %2i.\n",
+           (double)igraph_vector_max(&result), (int)igraph_vector_which_max(&result));
+
+    std::cout << "Vertex list" << std::endl;
+    for (int i=0; i<sizeof(t) ; ++i){
+        std::cout << "-" << i << ":" << VAS(&graph, "color", i) << std::endl;
+    }
+
+
+    igraph_matrix_t res;
+    igraph_matrix_init(&res, 0, 0);// (int)sizeof(t));
+    igraph_layout_bipartite(&graph, &types, &res, 3, 10, 100);
+
+    std::ofstream outfile;
+    outfile.open("output.txt");
+
+    for(int i = 0; i < igraph_matrix_nrow(&res);++i){
+        for(int j=0; j < igraph_matrix_ncol(&res); ++j){
+            outfile << MATRIX(res, i, j) << " ";
+        }
+        outfile << std::endl;
+    }
+
+    igraph_vector_destroy(&result);
+    igraph_vector_bool_destroy(&types);
+    igraph_destroy(&graph);
+
+    return 0;
 }
